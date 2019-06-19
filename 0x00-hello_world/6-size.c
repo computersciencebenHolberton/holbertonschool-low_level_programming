4a8f84a890a6cc8743e33d662158263f4f7e7486
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-19 20:08:20 +0000

    fk

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * number 6
+ *
+ *alway the way i like
+ */
+
+int main(void)
+
+{
+  
+  printf("Size of char: %c", sizeof(char));
+  printf("Size an  int: %d", sizeof(int));
+  printf("Size of a long int: %li", sizeof(long int));
+  printf("Size of a long long int: %lli", sizeof(long long int));
+  printf("Size of a float: %f", sizeof(float));
+
+  return 0;
+}
