ad83432b1f05cdae87e72c97276615fc546f3868
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-19 20:31:41 +0000

    fk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 printf("Size of char: %lu", sizeof(char));
 printf("Size an int: %lu", sizeof(int));
 printf("Size of a long int: %lu", sizeof(long int));
-printf("Size of a long long int: %lu", sizeof(long long int));
+printf("Size of a long long int: %lu", sizeof(longlong int));
 printf("Size of a float: %lu", sizeof(float));
 return (0);
 }
