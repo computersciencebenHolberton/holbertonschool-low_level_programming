8e804c8187205dafc1b6c299bf458cc848ddbcad
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-21 12:57:13 +0000

    base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
- 
 /**
- * main - lets try the world
+ * main - using the C
  *
  * Return: Always 0
  */
+int main(void)
+{
+for(int n = 0; n < 9; n++)
+        {
+		putchar((n % 10)+'0');
+        }
+return (0);
+}
