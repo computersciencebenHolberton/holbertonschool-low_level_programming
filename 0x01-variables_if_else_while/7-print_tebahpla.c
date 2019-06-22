843c1bb18ba850ccd18ac85dfab8d253f35d9630
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-22 17:46:14 +0000

    base 10

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
- * main - the show must do on
+ * main - number 7
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char n;
+char n;
 
-	for (n = 'z'; n >= 'a'; --n)
+	for (n = 'z'; n  >= 'a'; n--)
 	{
 		{
-		putchar('n');
+			putchar(n);
 		}
 	}
-	return (0);
+putchar('\n');
+return (0);
 }
