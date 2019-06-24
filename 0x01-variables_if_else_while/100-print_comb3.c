47ac260d4dea4dc33ea1594d97c5fc3111b7fdf0
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-24 15:36:36 +0000

    base 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - the challenge on 11
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int m;
+	int n;
+	for (i = '0'; i <= '8'; i++)
+		for (m = '1'; m <= '9'; m++)
+		{
+			{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar(i);
+	putchar(m);
+		}
+	putchar('\n')
+	return (0);
+}
