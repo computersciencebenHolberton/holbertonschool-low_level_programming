417f70dbf43234e08ccfee0ebdb4a75aef2e81dd
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-24 18:03:04 +0000

    base 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,19 @@ int i;
 int m;
 
 for (i = '0'; i <= '8'; i++)
-	for (m = '1'; m <= '9'; m++)
-		{
-			if (i < m)
-			{
-				if (i != '0' || m != '1')
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				putchar(i);
-				putchar(m);
-			}
-		}
+for (m = '1'; m <= '9'; m++)
+{
+if (i < m)
+{
+if (i != '0' || m != '1')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(i);
+putchar(m);
+}
+}
 putchar('\n');
 return (0);
 }
