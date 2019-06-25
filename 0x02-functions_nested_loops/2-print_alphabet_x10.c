8511402da4018e71347d363ce23581c066020c58
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-25 20:55:16 +0000

    0x02 function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**                                                                                                                  
+ * main - check the code for Holberton School students.                                                              
+ *                                                                                                                   
+ * Return: Always 0                                                                                                  
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+	}
+return (0);
+}
