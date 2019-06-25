d18ef31875cecb2e7674c832c31c2e19da4e76ea
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-25 14:57:03 +0000

    0x02 function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * main - its the 0x02-function
  *
@@ -7,15 +8,15 @@
 int main(void)
 {
 
-	putchar('H');
-	putchar('o');
-	putchar('l');
-	putchar('b');
-	putchar('e');
-	putchar('r');
-	putchar('t');
-	putchar('o');
-	putchar('n');
-	putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
