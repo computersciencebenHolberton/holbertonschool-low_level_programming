7c72eb31a1d998edd8061cb675c0b90679e99b55
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-25 14:35:18 +0000

    0x02 function

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**                                                                                                                
+ * main - the way you do 11                                                                                        
+ *                                                                                                                 
+ * Return: Always 0                                                                                                
+ */
+int main(void)
+{
+	int i;
+	int m;
+	int n;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+   
+		//if (i != '0' || m != '0')                                            //if (i < m)
+		for (m = '1'; m <= '8'; m++)
+      
+		{  
+			if (i != n) 
+				for (n = '2'; n <= '9'; n++)
+				{  
+            
+                                        
+					{
+                                                                      
+                                                                       
+					}
+                                                   
+					{
+						if (n != '0' , n != '0')
+							putchar(',');
+						putchar(' ');
+					}
+					putchar(i);
+					putchar(m);
+					putchar(n);
+				}
+    
+		}
+	}           
+            
+
+	putchar('\n');
+	return (0);
+}
