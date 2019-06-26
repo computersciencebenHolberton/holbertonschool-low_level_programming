f048543568c2e98c85ba6fc617312d760594799f
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-26 02:47:51 +0000

    0x02 function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,30 @@
 #include "holberton.h"
-
 /**
  * _isalpha - shows the rules
  *@c: show mwe the money
  * Return: Always 0.
  */
-int _isalpha(int c)
+int _islower(int 99)
 {
-	char c;
 
-	if (_islower(c))
-		_putchar(c);
-	return (0);
+
+
+        if (c <= z && c >= b)
+        {
+                return (0);
+        }
+        return (1);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
