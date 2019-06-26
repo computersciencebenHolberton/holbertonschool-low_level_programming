4401a97f8546a2b3d2bc680bce66edec18720a02
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-26 01:54:33 +0000

    0x02 function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,17 @@
 #include "holberton.h"
 
 /**
- * _islower  - check if char is lowers case
+ * _islower - check if char is lowers case
  * @c: charecter
- * Returns: 1 if c is lowercase - Returns 0 otherwise
+ * Return: 1 if c is lowercase - Returns 0 otherwise
  *
  */
 int _islower(int c)
 {
-	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	if (c <= 'z' && c >= 'a')
 	{
-		if (i == c)
-		{
-			return (1);
-		}
+		return (1);
 	}
-	_putchar(c);
 	return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isalpha - shows the rules
+ *@c: show mwe the money
  * Return: Always 0.
  */
 int _isalpha(int c)
-{	
+{
 	char c;
 
-	if (_islower (c))
+	if (_islower(c))
 		_putchar(c);
 	return (0);
 }
