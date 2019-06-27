269583cbcdddbc46fe5c78e9e89e2dd0760cab2d
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-27 17:34:16 +0000

    0x02 function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,16 @@
 #include "holberton"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	retrun (0);
+}
+{
