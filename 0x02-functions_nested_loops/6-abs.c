6bc5b5e3f8e1acf4ecfac2b54d501a609f9232ec
CompoutersciencebenHolberton <computersciencebenHolberton>
2019-06-27 00:44:08 +0000

    0x02 function

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,28 +1,20 @@
-#includeinum "holberton.h"
+#include "holberton.h"
 
 /**
  * main - the front door
  *@n: variable
  * Return: Always 0.
  */
-int _abs(int)
-{
-	int n;
+int _abs(int n)
 	{
 		if (n < 0)
 		{
-			(-1 x n = 1)
-			_putchar(n);
-			_putchar('\n');
+			n *= -1;
+			return(n);
 		}
-		if (n == 0)
+		else 
 		{
-			_putchar(n);
-			_putchar('\n');
+			return(n);
 		}
-		if (n > 0)
-		{
-			_putchar(n);
-                        _putchar('\n');
 }
 
