ecb07398195f4e7846b500a56d98772fdca222ba
computersciencebenHolberton <825@holbertonschool.com>
2019-07-02 11:37:55 +0000

    lets do it

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,9 +7,5 @@
  */
 void reset_to_98(int *n)
 {
-  int n=402;
-	int *p=&n;
-	
-	*p=98
-
+  *n=98;
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,9 @@
+
+
+
+int i;
+for (i = 0; s[i] != '\0'; i++)
+  {
+    putchar(s[i];
+	    }
+      putchar ('\n'); 
