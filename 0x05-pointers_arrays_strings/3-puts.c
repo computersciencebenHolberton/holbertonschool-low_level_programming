dd3cad3db1b27d1db1eafd144e5ef8e1e21bc758
= <computersciencebenHolberton>
2019-07-02 16:17:25 +0000

    string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,11 @@
 #include "holberton"
 
 /**
- * main - check the code for Holberton School students.
+ * void _puts - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+void _puts(char *str)
 for (i = 0; s[i] != '\0'; i++)
 {
   _putchar('\n');
