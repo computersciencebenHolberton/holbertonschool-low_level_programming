a871589d793dcde3af0d6508c13c689b969d3598
= <computersciencebenHolberton>
2019-07-02 14:44:32 +0000

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,13 @@
+#include "holberton.h"
 
+/**
+ * int _strlen - the way to use it
+ * @s:th
+ * Return: Always 0.
+ */
+int _strlen(char *s)
 
-for (i =0; s [i] != '\0'; i++)
 {
-
-}
+for (i = 0; s[i] != '\0'; i++)
 return (i);
+}
