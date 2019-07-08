e9309e9cde06170892f6753873456ef690ad2ee7
computersciencebenHolberton <825@holbertonschool.com>
2019-07-08 02:52:04 +0000

    f1

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+
+/**
+ * Main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
