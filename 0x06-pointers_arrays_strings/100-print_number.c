d0e9589f29e4c7532e18726c09e375ca7aa063ce
computersciencebenHolberton <825@holbertonschool.com>
2019-07-08 19:52:14 +0000

    f1

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,3 +5,14 @@
  *
  * Return: Always 0.
  */
+int y;
+if (n> 0)
+
+for (y = 10000000000; y > 0 ; y / = 10)
+  {
+    if (n % 10 != 0)
+      _putchar(n / y) % 10)+'0')
+if (n > 0)
+	n* == 1
+	else if (n < 0 )
+	  n* == 1;
