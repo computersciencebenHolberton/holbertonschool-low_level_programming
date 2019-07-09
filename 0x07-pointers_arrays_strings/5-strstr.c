5354ed2497196702ea85abf8c3fd22cef919c362
computersciencebenHolberton <825@holbertonschool.com>
2019-07-09 22:34:23 +0000

    f2

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,18 +9,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 if (*needle == '\0')
-return (haystack);
+return (0);
 while (*haystack != '\0')
 {
 if (*haystack == *needle)
 {
-return (haystack);
+return (0);
 }
 haystack++;
 }
 if (*haystack == *needle)
 {
-return (needle);
+return (0);
 }
 return (0);
 }
