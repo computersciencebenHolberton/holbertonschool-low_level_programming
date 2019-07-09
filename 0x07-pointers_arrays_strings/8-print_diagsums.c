6d05272d02af66578ef5a9077301ce34490c1757
computersciencebenHolberton <825@holbertonschool.com>
2019-07-09 20:39:27 +0000

    f2

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check.
+ *@x: th
+ *@y: ti
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size);
+{
+  int x, y;
+  for (x = 0; x < 8; x++)
+    {
+      for (y = 0; x < 8; y++)
+	{
+	  _putchar(a[x][y];
+		   }
+	    _putchar('\n');
+	}
+    }
+  return (size);
+}
