5a1aba5fbb1eba4bd999cc303fb4fbf9e2f77eff
computersciencebenHolberton <825@holbertonschool.com>
2019-07-09 22:19:39 +0000

    f2

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strstr - check the code for Holberton School students.
+ *@haystack: th
+ *@needle: the
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+if (*needle == '\0')
+return (haystack);
+while (*haystack != '\0')
+{
+if (*haystack == *needle)
+{
+return (haystack);
+}
+haystack++;
+}
+if (*haystack == *needle)
+{
+return (haystack);
+}
+return (0);
+}
