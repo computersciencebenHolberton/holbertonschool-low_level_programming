2849f4c8da813cace32b99e827cb936c1b4ac1a3
computersciencebenHolberton <825@holbertonschool.com>
2019-07-09 18:31:35 +0000

    f1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy: suf
+ * @n: the
+ * @dest: the size of the memory to print
+ * @src: the
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
