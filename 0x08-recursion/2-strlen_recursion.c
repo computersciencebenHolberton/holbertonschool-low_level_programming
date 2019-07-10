49cc63812660644a8e8fc5f076a7d595754aec1d
computersciencebenHolberton <825@holbertonschool.com>
2019-07-10 18:32:56 +0000

    f2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: thi
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
+}
