30cf7518821039ead9bac36a45fa28521f23e7bb
computersciencebenHolberton <825@holbertonschool.com>
2019-07-14 18:43:20 +0000

    f4

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - thi
+ *@argc: argc ment
+ *@argv: arry
+ *Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+while (argc--)
+{
+printf("%s\n", *argv++);
+}
+return (0);
+}
