fcc60cde1945dabc2d8e5ac408e431cc0b733807
computersciencebenHolberton <825@holbertonschool.com>
2019-07-14 18:24:33 +0000

    f4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - thi
+ *@argc: complic
+ *@argv: thi
+ *Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", (argc - 1));
+return (0);
+}
