4b8abe682c96d0c0b421dc1db13b24500c6e54f3
computersciencebenHolberton <825@holbertonschool.com>
2019-07-17 02:46:03 +0000

    f

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,17 @@
-#
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *free_grid - do gertting it to work
+ *@grid: the 2d array
+ *@height: the number of arrays
+ *Return
+ */
+void free_grid(int **grid, int height)
+{
+int n;
+
+for (n = 0; n < height; n++)
+free(grid[n]);
+free(grid);
+}
