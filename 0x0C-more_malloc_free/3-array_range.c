1e413996bf56299b1412b3c4c8fbf38d6ff5836e
computersciencebenHolberton <825@holbertonschool.com>
2019-07-18 21:25:52 +0000

    5

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- *array_range - arry 1
+ * array_range - array 1
  *@min:first
  *@max:end
- *Return:
+ *Return:NULL
  */
 int *array_range(int min, int max)
 {
@@ -14,7 +14,7 @@ int *ptr;
 if (min > max)
 return (NULL);
 cnt_range = max - min + 1;
-ptr = malloc(cnt_range * sizeof(*ptr));
+ptr = malloc(cnt_range *sizeof(*ptr));
 if (ptr == NULL)
 return (NULL);
 for (cnt1 = 0; cnt1 < cnt_range; min++, cnt1++)
