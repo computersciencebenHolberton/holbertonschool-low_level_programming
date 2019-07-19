12738c38eee9f7fb6d02add7613d51dce640c4f9
computersciencebenHolberton <825@holbertonschool.com>
2019-07-19 01:53:46 +0000

    5

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
