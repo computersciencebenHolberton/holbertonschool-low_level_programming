454e64bc032c9eaa0442eceb310c96fe32c2f534
computersciencebenHolberton <825@holbertonschool.com>
2019-07-23 00:40:04 +0000

    F5

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ *struct dog - name,age,owner
+ *@name:name
+ *@age:age
+ *@owner:owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+;
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
+void free_dog(dog_t *d);
+#endif
