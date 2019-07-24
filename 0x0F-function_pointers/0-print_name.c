04306ca95f944ec1543c45b43cd7c2ddb7602ec8
computersciencebenHolberton <825@holbertonschool.com>
2019-07-24 18:39:39 +0000

    F6

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 /**
- * print_name - char
- * @name: ponter
- * @f: f
- * Return: void
+ *print_name - char
+ *@name:ponter
+ *@f:f
+ *Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-  if (f)
-    {
-      f(name);
-    }
+if (f)
+{
+f(name);
+}
 }
