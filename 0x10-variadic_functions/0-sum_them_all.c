3c198a44d869e51582ca4fd400ac917eb47aa03b
computersciencebenHolberton <825@holbertonschool.com>
2019-07-27 18:45:08 +0000

    5

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *sum_them_all - parameters
+ *@n:constant
+ *Return:sum
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+va_list list;
+int sum = 0;
+if (n == 0)
+return (0);
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(list, int);
+}
+va_end(list);
+return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,9 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
