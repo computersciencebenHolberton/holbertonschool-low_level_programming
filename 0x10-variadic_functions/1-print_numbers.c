34875211f6037fd499552fcd39d88506ae124549
computersciencebenHolberton <825@holbertonschool.com>
2019-07-27 19:14:08 +0000

    5

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,21 @@
-#include "variadic_function.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ *print_numbers - pnumbers
+ *@n:int 
+ *Return:sum
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list valist;
+unsigned int i;
+va_start(valist, n);
+for (i = 0; i < n; i++)
+{
+if (i != 0 && separator != '\0')
+printf("%s", separator);
+printf("%d", va_arg(valist, int));
+}
+printf("\n");
+}
