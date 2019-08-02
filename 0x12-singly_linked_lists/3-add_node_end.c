d81de01ccbe1ce71e7b8ed78ff935c4b67fb7d47
computersciencebenHolberton <825@holbertonschool.com>
2019-08-02 01:29:14 +0000

    fixed

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+
+{
