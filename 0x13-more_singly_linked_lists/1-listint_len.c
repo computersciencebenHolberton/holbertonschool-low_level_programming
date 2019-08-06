a58ab7f1716642d6220209af2e92f5551276bd48
computersciencebenHolberton <825@holbertonschool.com>
2019-08-06 18:16:41 +0000

    0x13

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,16 @@
-!
+#include "lists.h"
+/**
+ *listint_len -list
+ *@h:pointer
+ *Return:number element
+ */
+
+size_t listint_len(const listint_t *h)
+{
+int i;
+for (i = 0; h != NULL; i++)
+{
+h = h->next;
+}
+return (i);
+}
