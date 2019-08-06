019763983cc089147758c23144463805a9f72e87
computersciencebenHolberton <825@holbertonschool.com>
2019-08-06 18:30:02 +0000

    0x13

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,18 @@
-!
+#include "lists.h"
+/**
+ **add_nodeint -new node
+ *@head:pointer
+ *@n:const
+ * Return:NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new = NULL;
+new = malloc(sizeof(listint_t));
+if (new == NULL && n != 0)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
+}
