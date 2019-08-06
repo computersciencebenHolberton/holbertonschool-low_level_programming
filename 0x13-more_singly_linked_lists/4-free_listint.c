a60929e635edfe2d26f844f0c1904026c78cddb4
computersciencebenHolberton <825@holbertonschool.com>
2019-08-06 21:20:46 +0000

    0x13

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,15 @@
-!
+#include "lists.h"
+/**
+ *free_listint -list
+ *@head:first
+ */
+void free_listint(listint_t *head)
+{
+listint_t *next;
+while (head)
+{
+next = head->next;
+free(head);
+head = next;
+}
+}
