f6d2482dce5ace124f85b97966823dadee29a1a1
computersciencebenHolberton <825@holbertonschool.com>
2019-08-06 18:42:52 +0000

    0x13

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,1 +1,30 @@
-!
+#include "lists.h"
+/**
+ *add_nodeint_end - add node to end of linked list (warning: slow!)
+ *@head: pointer to head struct pointer
+ *@n: int to add
+ *Return: pointer to new item, or NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, int n)
+{
+
+listint_t *new, *end;
+if (!head)
+return (NULL);
+new = malloc(sizeof(*new));
+if (!new)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (!*head)
+*head = new;
+else
+{
+end = *head;
+while (end->next)
+end = end->next;
+end->next = new;
+}
+return (new);
+}
+
