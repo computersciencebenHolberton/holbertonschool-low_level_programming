c05765b69370a274c62b834412bf011cc828fa40
computersciencebenHolberton <825@holbertonschool.com>
2019-08-07 00:58:49 +0000

    0x13

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,17 @@
-!
+#include "lists.h"
+/**
+ *free_listint2 -list
+ *@head:start of list
+ *Return:void
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *holder;
+while (head && *head)
+{
+holder = *head;
+*head = holder->next;
+free(holder);
+}
+}
+
