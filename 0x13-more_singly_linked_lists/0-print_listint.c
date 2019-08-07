db97b86232b3706c306038bf311904f9f8871a23
computersciencebenHolberton <825@holbertonschool.com>
2019-08-07 01:51:58 +0000

    0x13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,12 @@
 /**
  *print_listint - list elements
  *@h:type
- *Return: Number nodes
+ *Return:Number nodes
  */
 size_t print_listint(const listint_t *h)
 {
 int m;
-for (m = 0; h != NULL; m++)
+for (m = 0; h != 0; m++)
 {
 printf("%d\n", h->n);
 h = h->next;
