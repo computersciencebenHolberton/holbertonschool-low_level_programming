450666d4225b31dfb5095a9ae62d0f94b08b457c
computersciencebenHolberton <825@holbertonschool.com>
2019-08-07 01:37:40 +0000

    0x13

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,19 @@
-!
+#include "lists.h"
+/**
+ *pop_listint -it return value 
+ *@head:first
+ *Return:
+ */
+int pop_listint(listint_t **head)
+{
+int i;
+listint_t *next;
+if (!head || !*head)
+return (0);
+i = (*head)->n;
+next = (*head)->next;
+free(*head);
+*head = next;
+return (i);
+}
+
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,16 @@
-1
+#include "lists.h"
+/**
+ *get_nodeint_at_index -node in list
+ *@head:points to first 
+ *@index:item
+ *Return:point to index or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+if (index == 0)
+return (head);
+if (head)
+return (get_nodeint_at_index(head->next, index - 1));
+return (NULL);
+}
+
