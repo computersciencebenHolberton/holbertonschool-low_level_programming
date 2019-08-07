e3d8b9f13e9fd60e7dd6340aa2d4eff4634d0278
computersciencebenHolberton <825@holbertonschool.com>
2019-08-07 02:28:23 +0000

    0x13

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,1 +1,20 @@
-!
+#include "lists.h"
+/**
+ *reverse_listint -list
+ *@head:first
+ *Return:head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *prev = NULL, *next;
+if (!head)
+return (NULL);
+while (*head)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
+return (*head = prev);
+}
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,16 @@
-!
+#include "lists.h"
+/**
+ *sum_listint -sum of list
+ *@head:first
+ *Return:sum
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
